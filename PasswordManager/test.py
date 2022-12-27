4ca013c34bd26513063d0959ae9525d670527ee2
Pranav Pore <pranav@pranavpore.com>
2022-12-27 11:05:26 +0530

    deleted files

--- PasswordManager/test.py
+++ PasswordManager/test.py
@@ -3,7 +3,7 @@ import gnupg
 gpg = gnupg.GPG(gnupghome="/home/pranav/Data/development/Simple-Projects/PasswordManager/UserHome")
 f = open("unencrypted-file.txt", "rb")
 
-status = gpg.encrypt_file(f, recipients=["pranav@pranavpore.com"], output="encrypted.txt.gpg", always_trust=True)
+status = gpg.encrypt_file(f, recipients=["test@test.com"], output="encrypted.txt.gpg", always_trust=True)
 status.data
 
 print ('ok: ', status.ok)
