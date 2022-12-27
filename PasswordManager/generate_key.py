c373483598fbcacf8901c897c4def566133407d2
Pranav Pore <pranav@pranavpore.com>
2022-12-27 22:36:49 +0530

    updated code

--- PasswordManager/generate_key.py
+++ PasswordManager/generate_key.py
@@ -1,15 +1,15 @@
 import gnupg
 
 gpg = gnupg.GPG(gnupghome="/home/pranav/Data/development/Simple-Projects/PasswordManager/UserHome")
-input_data = gpg.gen_key_input(name_email='test@test.com', passphrase='passphrase')
+email = input("Enter your email: ")
+passphrase = input("Enter a new password for the key: ")
+
+input_data = gpg.gen_key_input(name_email=email, passphrase=passphrase)
 key = gpg.gen_key(input_data)
 print(key)
 
 ascii_armored_public_keys = gpg.export_keys(key.fingerprint)
-ascii_armored_private_keys = gpg.export_keys(key.fingerprint, True, passphrase="passphrase")
+ascii_armored_private_keys = gpg.export_keys(key.fingerprint, True, passphrase=passphrase)
 
-print(ascii_armored_private_keys)
-print(ascii_armored_public_keys)
-
-open("mykeyfile.asc", "w").write(ascii_armored_public_keys)
-open("mykeyfileprivate.asc", "w").write(ascii_armored_private_keys)
+open("UserHome/public.asc", "w").write(ascii_armored_public_keys)
+open("UserHome/private.asc", "w").write(ascii_armored_private_keys)
